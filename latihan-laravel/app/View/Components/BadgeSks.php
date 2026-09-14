66c58348e82e0cf06ceb7ae11d31dc8a6c1cb9b4
madztrad <yasinhuf@gmail.com>
2026-09-14 21:18:43 +0700

    mes2

--- latihan-laravel/app/View/Components/BadgeSks.php
+++ latihan-laravel/app/View/Components/BadgeSks.php
@@ -11,7 +11,7 @@ class BadgeSks extends Component
     /**
      * Create a new component instance.
      */
-    public function __construct()
+    public function __construct(public int $sks)
     {
         //
     }
--- latihan-laravel/resources/views/components/badge-sks.blade.php
+++ latihan-laravel/resources/views/components/badge-sks.blade.php
@@ -1,3 +1,5 @@
-<div>
-    <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
-</div>
+@if ($sks < 3)
+    <span class="badge bg-warning text-dark">{{ $sks }} SKS</span>
+@else
+    <span class="badge bg-success">{{ $sks }} SKS</span>
+@endif
