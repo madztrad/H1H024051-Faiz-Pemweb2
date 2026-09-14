ae518b0bcc1d851bf25bcf5b6b5499bc387c847b
madztrad <yasinhuf@gmail.com>
2026-09-14 21:14:17 +0700

    mes1

--- latihan-laravel/app/Http/Controllers/MatakuliahController.php
+++ latihan-laravel/app/Http/Controllers/MatakuliahController.php
@@ -6,28 +6,27 @@ use Illuminate\Http\Request;
 
 class MatakuliahController extends Controller
 {
-    public function index()
-    {
-        $daftarMatakuliah = [
-            ['kode' => 'TK213', 'nama' => 'Praktikum N1', 'sks' => 2023],
-            ['kode' => 'TK214', 'nama' => 'Pemweb', 'sks' => 2023],
-            ['kode' => 'TK215', 'nama' => 'IoT', 'sks' => 2024],
-        ];
-
-        return view('matakuliah.index', ['daftarMatakuliah' => $daftarMatakuliah]);
-    }
-
-    public function show(string $kode)
-    {
-        return view('matakuliah.show', ['kode' => $kode]);
-    }
-    public function cari(Request $request)
+    public function index(Request $request)
 {
-$kataKunci = $request->query('q', '');
-return response()->json([
-'kata_kunci' => $kataKunci,
-'metode' => $request->method(),
-'path' => $request->path(),
-]);
+    $kataKunci = $request->query('q', '');
+
+    $daftarMatakuliah = [
+        ['kode' => 'IF101', 'nama' => 'Algoritma', 'sks' => 3],
+        ['kode' => 'IF102', 'nama' => 'Basis Data', 'sks' => 4],
+        ['kode' => 'IF103', 'nama' => 'Jaringan Komputer', 'sks' => 2],
+        ['kode' => 'IF104', 'nama' => 'Pemrograman Web II', 'sks' => 3],
+        ['kode' => 'IF105', 'nama' => 'Kecerdasan Buatan', 'sks' => 3],
+    ];
+
+    if ($kataKunci !== '') {
+        $daftarMatakuliah = array_filter($daftarMatakuliah, function ($mk) use ($kataKunci) {
+            return str_contains(strtolower($mk['nama']), strtolower($kataKunci));
+        });
+    }
+
+    return view('matakuliah.index', [
+        'daftarMatakuliah' => $daftarMatakuliah,
+        'kataKunci' => $kataKunci,
+    ]);
 }
 }
--- latihan-laravel/resources/views/matakuliah/index.blade.php
+++ latihan-laravel/resources/views/matakuliah/index.blade.php
@@ -1,38 +1,49 @@
 @extends('layouts.app')
-@section('judul', 'Daftar Mahasiswa')
-<x-kartu-info judul="Informasi">
-Data pada halaman ini masih berupa array statis. Pada modul
-berikutnya data akan diambil dari basis data.
-</x-kartu-info>
+@section('judul', 'Daftar Matakuliah')
+
 @section('konten')
-<h1 class="h3 mb-4">Daftar Mahasiswa</h1>
-<table class="table table-bordered bg-white">
-<thead>
-<tr>
-<th>Kode</th>
-<th>Nama</th>
-<th>SKS</th>
-<th>Aksi</th>
-</tr>
-</thead>
-<tbody>
-@forelse ($daftarMatakuliah as $matakuliah)
-<tr>
-<td>{{ $matakuliah['kode'] }}</td>
-<td>{{ $matakuliah['nama'] }}</td>
-<td>{{ $matakuliah['sks'] }}</td>
-<td>
-<a href="{{ route('matakuliah.show',
-$matakuliah['kode']) }}" class="btn btn-sm btn-primary">
-Detail
-</a>
-</td>
-</tr>
-@empty
-<tr>
-<td colspan="4">Data belum tersedia</td>
-</tr>
-@endforelse
-</tbody>
-</table>
+    <h1 class="h3 mb-4">Daftar Matakuliah</h1>
+
+    <x-kartu-info judul="Informasi">
+        Data pada halaman ini masih berupa array statis. Pada modul
+        berikutnya data akan diambil dari basis data.
+    </x-kartu-info>
+
+    <form method="GET" action="{{ route('matakuliah.index') }}" class="mb-3">
+        <div class="input-group">
+            <input type="text" name="q" class="form-control"
+                   value="{{ $kataKunci ?? '' }}" placeholder="Cari matakuliah...">
+            <button type="submit" class="btn btn-primary">Cari</button>
+        </div>
+    </form>
+
+    <table class="table table-bordered bg-white">
+        <thead>
+            <tr>
+                <th>Kode</th>
+                <th>Nama</th>
+                <th>SKS</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($daftarMatakuliah as $matakuliah)
+                <tr>
+                    <td>{{ $matakuliah['kode'] }}</td>
+                    <td>{{ $matakuliah['nama'] }}</td>
+                    <td><x-badge-sks :sks="$matakuliah['sks']" /></td>
+                    <td>
+                        <a href="{{ route('matakuliah.show', $matakuliah['kode']) }}"
+                           class="btn btn-sm btn-primary">
+                            Detail
+                        </a>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="4">Data belum tersedia</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
 @endsection
