8898fa04178caa7b97648756cf713fa2bd92db91
madztrad <yasinhuf@gmail.com>
2026-09-22 14:26:27 +0700

    sebelum tugas

---
+++ latihan-laravel/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+use App\Http\Controllers\Api\MahasiswaController;
+use Illuminate\Support\Facades\Route;
+Route::get('/status', function () {
+ return response()->json([
+ 'sukses' => true,
+ 'pesan' => 'API Pemweb II aktif',
+ 'waktu' => now()->toIso8601String(),
+ ]);
+});
+Route::apiResource('mahasiswa', MahasiswaController::class);
